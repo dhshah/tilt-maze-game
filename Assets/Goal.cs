e5d41fac7771a7aa7593e7464dc64a586aace432
Darshan <dhshah11@terpmail.umd.edu>
2017-02-08 19:47:48 -0500

    game

---
+++ Assets/Goal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+	private Rigidbody rb;
+
+	public bool hasWon = false;
+
+  void Start() {
+      rb = GetComponent<Rigidbody>();
+  }
+
+	// Update is called once per frame
+	void Update () {
+		if (hasWon) {
+			transform.Translate(0f, 0.1f, 0f);
+			if (transform.position.y > 15) {
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.name == "Goal") {
+			rb.isKinematic = true;
+			hasWon = true;
+		}
+	}
+}
---
+++ Assets/Tilt.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilt : MonoBehaviour {
+
+	public float maxRotation = 10f;
+	public float speed = 0.5f;
+	public Vector3 currentRot;
+
+	public float xRot = 0f;
+	public float zRot = 0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		currentRot = GetComponent<Transform>().eulerAngles;
+
+		if (zRot > -maxRotation && Input.GetAxis("Horizontal") > 0.2f) {
+			zRot -= speed;
+		}
+		if (zRot < maxRotation && Input.GetAxis("Horizontal") < -0.2f) {
+			zRot += speed;
+		}
+		if (xRot < maxRotation && Input.GetAxis("Vertical") > 0.2f) {
+			xRot += speed;
+		}
+		if (xRot > -maxRotation && Input.GetAxis("Vertical") < -0.2f) {
+			xRot -= speed;
+		}
+
+		transform.eulerAngles = new Vector3(xRot, 0, zRot);
+	}
+
+	private float getDiff(float deg) {
+		if (deg > 180f)
+			return 360f - deg;
+		else
+			return deg;
+	}
+
+}
